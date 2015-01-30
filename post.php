0073a3b44c0932f922523d0e02aca2adb27c5932
Cameron Preito <cameotheman@yahoo.com>
2015-01-30 15:03:11 -0800

    next commit

---
+++ post.php
@@ -0,0 +1,4 @@
+<?php
+    require_once(__DIR__ . "/view/header.php");
+    require_once(__DIR__ . "")
+    require_once(_DIR__ . "/view/footer.php");
---
+++ view/navigation.php
@@ -0,0 +1,8 @@
+<?php  
+    require_once(__DIR__ . "/../model/config.php")
+?>
+<nav>
+    <ul>
+        <li><a href="<?php echo $path . "post.php"?>">Blog post Form</a></li>
+    </ul>
+</nav>
