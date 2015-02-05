f3e67a28c731174b0ec477ed714ba93b76386c28
Cameron Preito <cameotheman@yahoo.com>
2015-02-05 15:02:43 -0800

    next commit

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,17 +1,17 @@
 <?php
     require_once(__DIR__ . "/../model/database.php");
     
-    $connection = new mysqil($host, $username, $password, $database);
+    $connection = new mysqli($host, $username, $password, $database);
     
     $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
     $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
 
-    $query = $connection->query("INSERT INTRO posts SET title = '$title', post = '$post'");
+    $query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'");
     
     if($query) {
         echo "<p>Successfully inserted post: $title</p>";
     } else {
-          echo "<p>$connection->error</p> ";
+          echo "<p>$connection->error</p>";
     }
     
     $connection->close(); 
