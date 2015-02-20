c0162a7637f4b7d66d80ee8f0407f82d3c655f30
Cameron Preito <cameotheman@yahoo.com>
2015-02-19 17:50:36 -0800

    next commit

--- view/register-form.php
+++ view/register-form.php
@@ -2,9 +2,9 @@
     require_once(__DIR__ . "/../model/config.php");
 ?>
 
-<h1>Register</h1>
+<h1>Login/h1>
 
-<form method="post" action="<?php echo $path . "controller/create-user.php"; ?>">
+<form method="post" action="<?php echo $path . "controller/login-user.php"; ?>">
     <div> 
         <label for="email">Email: </label>
         <input type="text" name="email" />
